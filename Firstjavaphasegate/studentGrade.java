797756032ec187d4681010d0b0a547a9882303e3
TosinAdewuyi19 <adewuyitosin19@gmail.com>
2024-12-03 16:35:55 +0100

    phase

--- Firstjavaphasegate/studentGrade.java
+++ Firstjavaphasegate/studentGrade.java
@@ -9,7 +9,10 @@ Scanner input = new Scanner(System.in);
 
 
 int numberofstudents = 1;
-int numberofsubjects = 1;  
+int numberofsubjects = 1;
+double average = 0;
+double totalscore = 0;
+  
 
 
 
@@ -22,17 +25,24 @@ int[][] array = new int[numberofstudents][numberofsubjects];
 			numberofsubjects = input.nextInt();
 			System.out.println("Saving >>>>>>>>>>>>>>>");
 			System.out.println("Saved successfully");
-			
+														
+
 
 	for (int count = 0; count < numberofstudents; count++) {
 		for (int counter = 0; counter < numberofsubjects; counter++) {
 			System.out.println("Entering score for student " + count +" >>> ");
 			System.out.print("Enter score for subject " + counter + ": ");
-			int score = input.nextInt();}
-
-			//System.out.println("Saving >>>>>>>>>>>>>>>");
+			int score = input.nextInt();
 			System.out.println("Saved successfully");
-
+	
+			for (int x = 0; x < score; x++);
+				totalscore += score; 
+				average = totalscore / numberofsubjects;  
+}
+			System.out.println("==================================================");
+			System.out.println("STUDENT: %.2f%n, %.2f%n, %n%d\t count \t total \taverage \tPOS");
+			System.out.println("==================================================");	
+				
 }
     }
 } 
