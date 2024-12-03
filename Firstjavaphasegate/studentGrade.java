f729c88249af2bb5f16cf5c3e968245567900690
TosinAdewuyi19 <adewuyitosin19@gmail.com>
2024-12-03 12:50:14 +0100

    phase

--- Firstjavaphasegate/studentGrade.java
+++ Firstjavaphasegate/studentGrade.java
@@ -1,26 +1,29 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-public class stdentGrade {
+public class studentGrade {
 
 public static void main(String[] args) {
+	int numberOfStudents = 0;
+	int numberOfSubject = 0;
+
+
+int [][] studentGrade = new int[numberOfStudents][numberOfSubject]; 
+	
 	Scanner input = new Scanner(System.in);
+		for (int count = 0; count < studentGrade; count++) {
+			for (int counter = 0; counter < studentGrade; counter++) {
+				System.out.print("How many students do you have?: ");
+					numberOfStudents = input.nextInt();
 
-ArrayList<Integer> numberOfStudents = new ArrayList();
-ArrayList<Integer> numberOfSubjects = new ArrayList();
-ArrayList<Integer> studentScores = new ArrayList();
+				System.out.print("How many subject dothey offer?: ");
+					numberOfSubject = input.nextInt();
 
-
-public static void studentInfo(ArrayList<String> numberOfStudents,ArrayList<Integer>  numberOfSubjects,ArrayList<Integer> studentScores) {
-System.out.print("How many student do you have?: ");
-numberOfStudent.add = input.nextInt();
-System.out.print("How many subject do they offer?: ");
-numberOfSubjct.add = input.nextInt();
-System.out.println("Saving >>>>>>>>>>>>");
-System.out.println("Saved successfully");
-
-	}
+}
+				
+	
+}
     }
- 
+} 
 
 
 
