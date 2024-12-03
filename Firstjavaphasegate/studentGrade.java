087591c4653f9a4030bb834ad2fa07a8556300fa
TosinAdewuyi19 <adewuyitosin19@gmail.com>
2024-12-03 10:54:52 +0100

    phasegate

---
+++ Firstjavaphasegate/studentGrade.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class stdentGrade {
+
+public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+
+ArrayList<Integer> numberOfStudents = new ArrayList();
+ArrayList<Integer> numberOfSubjects = new ArrayList();
+ArrayList<Integer> studentScores = new ArrayList();
+
+
+public static void studentInfo(ArrayList<String> numberOfStudents,ArrayList<Integer>  numberOfSubjects,ArrayList<Integer> studentScores) {
+System.out.print("How many student do you have?: ");
+numberOfStudent.add = input.nextInt();
+System.out.print("How many subject do they offer?: ");
+numberOfSubjct.add = input.nextInt();
+System.out.println("Saving >>>>>>>>>>>>");
+System.out.println("Saved successfully");
+
+	}
+    }
+ 
+
+
+
+
+
+
+
+
+	
+ 
