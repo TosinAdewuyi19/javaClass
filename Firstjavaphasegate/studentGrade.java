2e8d10bbad4b36f65342224537b3b6ffeed8bc5d
TosinAdewuyi19 <adewuyitosin19@gmail.com>
2024-12-04 06:40:09 +0100

    phase

--- Firstjavaphasegate/studentGrade.java
+++ Firstjavaphasegate/studentGrade.java
@@ -8,42 +8,50 @@ public static void main(String[] args) {
 Scanner input = new Scanner(System.in);
 
 
-int numberofstudents = 1;
+/*int numberofstudents = 1;
 int numberofsubjects = 1;
 double average = 0;
-double totalscore = 0;
-  
-
-
+double totalscore = 0;*/
 
 		
-int[][] array = new int[numberofstudents][numberofsubjects];
 
-			System.out.println("How many students do you have?: ");
-			numberofstudents = input.nextInt();
-			System.out.println("How many subjects do they offer?: ");
-			numberofsubjects = input.nextInt();
-			System.out.println("Saving >>>>>>>>>>>>>>>");
-			System.out.println("Saved successfully");
+
+
+		System.out.println("How many students do you have?: ");
+			int numberofstudents = input.nextInt();
+		System.out.println("How many subjects do they offer?: ");
+			int numberofsubjects = input.nextInt();
+		System.out.println("Saving >>>>>>>>>>>>>>>");
+		System.out.println("Saved successfully");
+			
+			int[][] scores = new int[numberofstudents][numberofsubjects];
+			double[] average = new double[numberofstudents]; 
 														
 
 
-	for (int count = 0; count < numberofstudents; count++) {
-		for (int counter = 0; counter < numberofsubjects; counter++) {
-			System.out.println("Entering score for student " + count +" >>> ");
-			System.out.print("Enter score for subject " + counter + ": ");
-			int score = input.nextInt();
-			System.out.println("Saved successfully");
-	
-			for (int x = 0; x < score; x++);
-				totalscore += score; 
-				average = totalscore / numberofsubjects;  
-}
+		for (int count = 0; count < numberofstudents; count++) {
+			double totalscore = 0;
+			System.out.println("\nEntering score for student " + (count + 1) + " >>> ");
+			for (int counter = 0; counter < numberofsubjects; counter++) {
+				System.out.print("Enter score for subject " + (counter + 1) + ": ");
+				int score = input.nextInt();
+		
+				scores[count][counter] = score;
+				totalscore += score;
+			}
+					
+				average[count] = totalscore / numberofsubjects;
+		
+		}		
+				
+				for (int subject = 0; subject < numberofstudents; subject++)
+						System.out.print("SUB " + (subject + 1)); 
+		
 			System.out.println("==================================================");
-			System.out.println("STUDENT: %.2f%n, %.2f%n, %n%d\t count \t total \taverage \tPOS");
+			System.out.printf("%n%s %n%s %n%s %n%s\n",  "STUDET \tsubject \tTOT\taverage \tPOS");
 			System.out.println("==================================================");	
 				
-}
+
     }
 } 
 
