1bae9c43a1ab967f086ed0efd87afb39ba6d89b9
TosinAdewuyi19 <adewuyitosin19@gmail.com>
2024-12-05 07:13:26 +0100

    phase

--- Firstjavaphasegate/creditCardValidator.java
+++ Firstjavaphasegate/creditCardValidator.java
@@ -1,39 +1,60 @@
 import java.util.Scanner;
+import java.util.Arrays;
+
 public class creditCardValidator {
 public static void main(String[] args) {
 Scanner input = new Scanner(System.in);
+
 	System.out.print("Enter creditcard numebr: ");
 	String cardNumber = input.nextLine();
-		int[] digits = new int[cardNumber.length];
-		for (int count = 0; count <cardNumber.length; count++){
-			digits(count) = Character.getCardValue(cardNumber.charAt());
+	
+		Strig cardType = getCardType(cardNumber);
+		boolean isValid = isValidCardNumber(cardNumber);
 		
-	 String cardType = getCardType(cardNumber);
-        System.out.println("Card Number: " + cardNumber + ", Type: " + cardType);
-    }
+			System.out.println("card Type: " + cardType);
+			System.out.println("Validity: " + (isValid? "Valid" : "Invalid"));
+	}
+
+}
  
 	
-public static String getCardType(String cardNumber) {
+public static string getCardType(String cardNumber) {
 	int length = cardNumber.length();
-	if (length > 13 && length < 16){
-		   System.out.print("Invalid card");
-	}
-	else if (cardNumber.startsWith("4") && (length == 13 || length == 16)) {
-			System.out.println("VisaCards");		
-	}
-	else if (cardNumber.startsWith("5") && (length == 13 || length == 16)) {
-			System.out.println("MasterCards");
-	}
-	else if (cardNumber.startsWith("37") && (length == 13 || length == 16)) {
-			System.out.println("American Expresss Cards");
-	}
-	else if (cardNumber.startsWith("6") && (length == 13 || length == 16)) {
-			System.out.println("Discover cards");
+
+	if (cardNumber.startsWith("4") && (length == 13 || length == 16)) {
+			return "VisaCards";		
+	
+	}else if (cardNumber.startsWith("5") && (length == 13 || length == 16)) {
+			return "MasterCards";
+	
+	}else if (cardNumber.startsWith("37") && (length == 13 || length == 16)) {
+			return "American Expresss Cards";
+	
+	}else if (cardNumber.startsWith("6") && (length == 13 || length == 16)) {
+			return "Discover cards";
 	}else {
-			System.out.println("Invalid card");
+			return "Invalid Card Type";
 	}
 }
+public static boolean isValidCardNumber(String cardNumber) {
+	int[] digits = new int[cardNumber.length()];
+	for (int count = 0; count <cardNumber.length; count++){
+		digits(count) = Character.getNumericValue(cardNumber.charAt(count));
+	}
+	
+	for (int count = digits.length - 2; count >= 0; count -= 2){
+		digits[count] *=2;
+		if (digits[count] >9){
+			digits[count] = digits[count] / 10 +digits[count] %10;
+		}
+	}
+	
+	int sum = 0;
+	for (int count = digits.length - 1; count >=0; count -=2){
+		sum += Character.getNumericValue(cardNumber.charAt(count));
+
 }
- 
+
+}
  
 	 
